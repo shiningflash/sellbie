4e7ed359e9181aaa608e702b00531e2f73ce8e7d
Amirul Islam <amirulislamalmamun@gmail.com>
2023-10-15 15:52:49 +0600

    defined router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,39 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import Product from "./pages/Product.tsx"
+import Transaction from "./pages/Transaction.tsx"
+import Profile from "./pages/Profile.tsx"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+        <Product />
+    ),
+  },
+  {
+    path: "/transaction",
+    element: (
+        <Transaction />
+    ),
+  },
+
+  //profile page with dynamic id parameter
+  {
+    path: "/profile/:id",
+    element: <Profile />,
+  },
+  {
+    path: "/product/:id",
+    element: <Product />,
+  },
+])
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+      <RouterProvider router={router} />
+      <App />
+  </React.StrictMode>
 )
